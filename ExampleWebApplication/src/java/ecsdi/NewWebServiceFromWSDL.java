acbdabae4cd64f51a8d41e0556baa75cabac4c25
Sergio Alvarez-Napagao <tranchis@gmail.com>
2013-05-06 17:58:21 +0200

    introducing jena example

--- ExampleWebApplication/src/java/ecsdi/NewWebServiceFromWSDL.java
+++ ExampleWebApplication/src/java/ecsdi/NewWebServiceFromWSDL.java
@@ -4,6 +4,11 @@
  */
 package ecsdi;
 
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.util.FileManager;
+import com.owl_ontologies.ecsdiservices.CentroMedicoType;
+import java.io.InputStream;
 import javax.jws.WebService;
 
 /**
@@ -14,8 +19,42 @@ import javax.jws.WebService;
 public class NewWebServiceFromWSDL {
 
     public com.owl_ontologies.ecsdiservices.CentroMedicoType getOutputCentroMedico(java.lang.String inputNombreCiudad) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+
+        return new CentroMedicoType();
     }
-    
+
+    public static void main(String args[]) {
+        NewWebServiceFromWSDL ws = new NewWebServiceFromWSDL();
+        ws.readOwl();
+    }
+
+    private void readOwl() {
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // use the FileManager to find the input file
+        InputStream in = FileManager.get().open("ECSDIOntology.owl");
+        if (in == null) {
+            throw new IllegalArgumentException(
+                    "File not found");
+        }
+
+        // read the RDF/XML file
+        model.read(in, null);
+
+        // write it to standard out
+        model.write(System.out);
+
+        String queryString = "PREFIX ecsdi:<http://www.owl-ontologies.com/ECSDI.owl>\n"
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+                + "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
+                + "SELECT ?indiv WHERE { ?indiv rdf:type ecsdi:Hospital  }";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        ResultSet results = qexec.execSelect();
+        // Output query results	
+        ResultSetFormatter.out(System.out, results, query);
+    }
 }
